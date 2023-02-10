3a538ccf7f2529b4564cfd8f0fc98a000b27c885
rami <124908204+FaZehulk@users.noreply.github.com>
2023-02-10 09:28:51 +0200

    Add files via upload

---
+++ progect.py
@@ -0,0 +1,53 @@
+                '''pisi'''
+# by rami
+'''pisi is a siri but on a laptop or ps'''
+
+def ins.():
+
+    print ("let me show you how this works if you tried this just press enter on your key_board to skip ")
+    var1=str (input) ("write hi pisi to wake pisi up ")
+    var2=str (input) ("write hey pisi can you open ...... , and it needs to be from these aps ,"fifa , whatsapp")
+    
+    if var2 == "fifa" :
+        print ('''_____________
+                      0   
+               play 
+               players          
+                         
+              ____fifa_____________''')
+    elif var2 == "whatsapp" :
+        print ('''to :- __________
+                                  
+                                  
+                      (            )send''')
+    else :
+        print ("this app is not downloaded")
+ins.()
+
+def start( ):
+     var3=str (input) ("write hi pisi to wake pisi up ")
+    var4=str (input) ("write hey pisi can you open ...... , and it needs to be from these aps ,"fifa , whatsapp,call dad")
+    
+    if var4 == "fifa" :
+        print ('''_____________
+                      0   
+               play 
+               players          
+                         
+              ____fifa_____________''')
+    elif var4 == "whatsapp" 
+        print ('''to :- __________
+                                  
+                                  
+                      (            )send''')
+    else :
+        print ("this app is not downloaded")
+
+    elif var4 == "call dad":
+        print ('''  dad
+                    caling...
+                    
+                          p----p
+                    p---p  0''')
+
+start ()
